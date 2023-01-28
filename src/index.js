0c36dfa22259d667e5ae60a28a2147067afb76f0
anandu <sonysandr@gmail.com>
2023-01-28 17:08:55 +0530

    Star Rating component using useState hook completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const createArray = (length) => [...Array(length)]
+
+function Star({ selected = true , onSelect }) {
+  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
+}
+
+function StarRating({totalStars}) {
+  const [selectedStars, setSelectedStars] = useState(0)
+  return (
+    <>
+    {createArray(totalStars).map((n, i) => 
+  (<Star 
+  key={i}
+  selected = {selectedStars>i}
+  onSelect={()=>setSelectedStars(i+1)}
+  
+   />))}
+   <p>Rating {selectedStars} of {totalStars}</p>
+   
+   </>
+   
+   )
+}
+
+function App() {
+  return <StarRating totalStars={10} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
